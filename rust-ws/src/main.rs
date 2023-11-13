0c118728e81a73d3a9c337c7cd0978158fbf06da
Hanson <105476@grundfos.com>
2023-11-13 17:53:08 +0800

    1

--- rust-ws/src/main.rs
+++ rust-ws/src/main.rs
@@ -1,3 +1,10 @@
 fn main() {
-    println!("Hello, world!");
+    for number in (1..4).rev() {
+        println!("{number}!");
+    }
+
+    for number in (1..=4).rev() {
+        println!("{number}!");
+    }
+    println!("LIFTOFF!!!");
 }
