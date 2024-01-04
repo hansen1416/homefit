2812acb540e21530c18ad34dd559816473fe763b
Hanson <105476@grundfos.com>
2024-01-04 09:30:30 +0800

    1

--- web-oldpose/src/App.svelte
+++ web-oldpose/src/App.svelte
@@ -1,6 +1,22 @@
 <script>
 	import { Router, Route } from "svelte-routing";
 	import Home from "./routes/Home.svelte";
+	import { websocket } from "./store/websocketStore";
+	import WebSocketClient from "./lib/WebSocketClient";
+
+	let socket = new WebSocketClient();
+	// When you write $websocket, you're essentially saying, "Get the current value from the store named websocket."
+	$websocket = socket; // Assign the instance to the store
+
+	// Connect to the websocket
+	socket.connect({ url: 'ws://localhost:3333' });
+
+	// Send messages or receive events
+	socket.sendMessage('Hello, server!');
+	socket.on('message', (event) => {
+		console.log(event.data);
+	});
+
 </script>
 
 <Router>
